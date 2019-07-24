999c09bd4c8b83ef0d711eddcb0414a3ef7709a4
hectormata <hector.mata9@gmail.com>
2019-07-24 16:33:17 -0400

    Added some new css

---
+++ Scripts/NotReady/countButtonClicks.js
@@ -0,0 +1,37 @@
+const fileLoc = "\"/Data/buttonsCount.json\"";
+var obj = {
+    table:[]
+};
+
+obj.table.push({emailCount:0, githubCount:0, linkedinCount:0});
+
+var json = JSON.stringify(obj);
+
+var fs = require("fs");
+fs.writeFile(fileLoc, json, "UTF-8", callback);
+
+fs.readFile(fileLoc, "UTF-8", function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+        obj = JSON.parse(data); //now it an object
+        obj.table.push({emailCount:1,githubCount: 2, linkedinCount:3}); //add some data
+        json = JSON.stringify(obj); //convert it back to json
+        fs.writeFile(fileLoc, json, "UTF-8", callback); // write it back
+    }
+});
+
+document.getElementById("emailButton").addEventListener("click", function () {
+
+    fs.readFile(fileLoc, "UTF-8",function(err,content){
+        if(err) throw err;
+        var parseJson = JSON.parse(content);
+        for (i=0; i <11 ; i++){
+            parseJson.table.push({emailCount:i, githubCount:i*i,linkedinCount:i++})
+        }
+        fs.writeFile(fileLoc,JSON.stringify(parseJson),function(err){
+            if(err) throw err;
+        })
+    });
+
+});
