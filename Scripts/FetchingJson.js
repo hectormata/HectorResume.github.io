10eef281ef9f741bb400dfda121bbbbc337fe1c3
hectormata <hector.mata9@gmail.com>
2019-10-04 17:47:09 -0400

    Create FetchingJson.js

---
+++ Scripts/FetchingJson.js
@@ -0,0 +1,14 @@
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send();
+}
+fetchJSONFile("https://api.github.com/users/github/Data/JobInfo.json",console.log)
